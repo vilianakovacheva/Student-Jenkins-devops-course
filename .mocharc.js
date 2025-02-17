f4688a19976ee5453bc0188333a50295e60ed3ee
vilianakovacheva <vilianakovacheva04@gmail.com>
2025-02-17 09:11:15 +0200

    test 4

--- .mocharc.js
+++ .mocharc.js
@@ -1,4 +1,4 @@
-// .mocharc.js - Mocha config file some test change 3
+// .mocharc.js - Mocha config file some test change 4
 const {colors, symbols} = 
   require('mocha/lib/reporters/base');
 // Change the default console UI colors
